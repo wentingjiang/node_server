ba4c6e8320e6b05056429bfd9616f4c4a9933c19
chloe <wentingjiang@outlook.com>
2021-03-14 22:57:10 +0800

    stop using body-parser

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 
 app.use(express.static(path.join(__dirname, '/views')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.text())
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/hello', (req, res) => {
 	res.send('hello world')
+})
+
+app.post('/testPostJson', (req, res)=>{
+	console.log(req.query)
+	let obj = req.body
+	console.log(obj.a)
+	res.send('testPostJson')
 })
 
 app.listen(app.get('port'), function () {
